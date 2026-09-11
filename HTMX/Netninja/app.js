5a2a5d4ae72f6fc325904b9623f0c3efba5a1ed2
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-09-12 03:29:28 +0800

    Lesson 3 done

--- HTMX/Netninja/app.js
+++ HTMX/Netninja/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import createHomepageTemplate from './views/index.js';
+import createListTemplate from './views/list.js';
 
 const app = express();
 app.use(express.urlencoded({extended: false}))
@@ -9,6 +10,10 @@ app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.send(createHomepageTemplate()).status(200);
+})
+
+app.get('/books', (req, res) => {
+    res.send(createListTemplate());
 })
 
 app.listen(5050, () => {
