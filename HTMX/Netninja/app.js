164dbceab3e371020182b6759fc723d767264f66
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-09-12 02:54:22 +0800

    add more dependencies: nodemon, morgan

--- HTMX/Netninja/app.js
+++ HTMX/Netninja/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.urlencoded({extended: false}))
-
 app.use(express.static('public'));
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.send({
