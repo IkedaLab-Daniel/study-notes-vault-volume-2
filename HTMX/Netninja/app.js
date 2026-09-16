55e9bb528f6ba72bcfe16bea1a6be169fdf5331b
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-09-16 23:27:12 +0800

    Lesson 5

--- HTMX/Netninja/app.js
+++ HTMX/Netninja/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import createHomepageTemplate from './views/index.js';
 import createListTemplate from './views/list.js';
+import BOOKS_DATA from './data/data.js';
 
 const app = express();
 app.use(express.urlencoded({extended: false}))
@@ -14,6 +15,21 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.send(createListTemplate());
+})
+
+app.post('/books', (req, res) => {
+    const {title, author} = req.body;
+    const id = Math.random().toString();
+
+    BOOKS_DATA.push(
+        {
+            id,
+            title,
+            author
+        }
+    );
+
+    res.send(`<li>${title}, ${author}</li>`).status(201);
 })
 
 app.listen(5050, () => {
